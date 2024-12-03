16a4b49b3882af5baab632593a5a4da034e55382
DawidMadryVWP <dawid.madry1@vw-group.pl>
2024-12-03 13:44:40 +0100

    aggregating data

--- SparkProject/main.py
+++ SparkProject/main.py
@@ -7,29 +7,37 @@ spark = SparkSession.builder \
 
 datasource1 = spark.read.csv(
     "merged_data.csv",
-    sep=";",        # Separator w pliku
-    header=False,    # Plik zawiera nagłówki
-    inferSchema=True # Automatyczne wykrycie typów kolumn
+    sep=";",        # separator
+    header=False,    # nagłówki
+    inferSchema=True # wykrycie typów kolumn
 )
 
-reduced_datasource1 = (datasource1.select(f.col("_c0").alias("player_id"),
-                                 f.col("_c7").alias("player_positions"),
-                                 f.col("_c10").alias("value_eur"),
-                                 f.col("_c11").alias("wage_eur"),
-                                 f.col("_c12").alias("age"),
-                                 f.col("_c16").alias("league_id"),
-                                 f.col("_c17").alias("club_team_id"),
-                                 f.col("_c18").alias("club_name"),
-                                 f.col("_c25").alias("nationality_name")))
+selected_columns = (datasource1.select(f.col("_c0").alias("player_id"),
+                                       f.col("_c7").alias("player_positions"),
+                                       f.col("_c10").alias("value_eur"),
+                                       f.col("_c11").alias("wage_eur"),
+                                       f.col("_c12").alias("age"),
+                                       f.col("_c16").alias("league_id"),
+                                       f.col("_c17").alias("club_team_id"),
+                                       f.col("_c18").alias("club_name"),
+                                       f.col("_c25").alias("nationality_name"))
+                    )
 
-selected_datasource1 = (
-    reduced_datasource1
-    .groupBy("club_team_id")
-    .agg(f.count("player_id").alias("total_players"))
-    .where(f.col("total_players") >= 11)  # Filtrowanie klubów z co najmniej 11 zawodnikami
-)
+agg_players = (selected_columns.groupBy("league_id", "club_team_id")
+               .agg(f.count("player_id").alias("total_players"))
+               .where(f.col("total_players") >= 11)
+               )
 
-nationality = reduced_datasource1
+agg_leagues = (agg_players.groupBy("league_id")
+               .agg(f.count("club_team_id").alias("total_clubs"))
+               .where(f.col("total_clubs") >= 10)
+               )
 
-reduced_datasource1.show()
+reduced_data = (selected_columns
+                .join(agg_leagues, on="league_id", how="inner")
+                )
+
+
+
+selected_columns.show()
 selected_datasource1.show()
